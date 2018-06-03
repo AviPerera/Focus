0202fcd792d00511a6a58fc9407b4bd1be303c18
Avishka Perera <avi.v.perera@icloud.com>
2018-06-03 20:35:56 +1000

    side panel animation added

--- Focus/ViewController.swift
+++ Focus/ViewController.swift
@@ -10,16 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sideMenuConstraint: NSLayoutConstraint!
+    
+    var isSideMenuHidden = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //initialize side panel with -175
+        sideMenuConstraint.constant = -175
+        
     }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func sideBarBtnPresses(_ sender: UIBarButtonItem) {
+        
+        if (isSideMenuHidden)
+        {
+            sideMenuConstraint.constant = 0
+            
+            //Add animation to the side panel
+            UIView.animate(withDuration: 0.3, animations: {self.view.layoutIfNeeded()})
+        }
+        else
+        {
+            sideMenuConstraint.constant = -175
+            //Add animation to the side panel
+            UIView.animate(withDuration: 0.3, animations: {self.view.layoutIfNeeded()})
+        }
+        isSideMenuHidden = !isSideMenuHidden
+    }
+    
 }
 
