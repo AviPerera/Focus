24d2a7b6c056be33c52a04f79006136aba823913
Avishka Perera <avi.v.perera@icloud.com>
2018-06-03 23:42:32 +1000

    Add tableview and study tasks view controller

---
+++ Focus/Study Plan/AddStudyTasksViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AddStudyTasksViewController.swift
+//  Focus
+//
+//  Created by Avi Perera on 3/6/18.
+//  Copyright © 2018 Avishka Perera. All rights reserved.
+//
+
+import UIKit
+
+class AddStudyTasksViewController: NSObject {
+
+}
---
+++ Focus/Study Plan/StudyPlanTableView.swift
@@ -0,0 +1,21 @@
+//
+//  StudyPlanTableView.swift
+//  Focus
+//
+//  Created by Avi Perera on 3/6/18.
+//  Copyright © 2018 Avishka Perera. All rights reserved.
+//
+
+import UIKit
+
+class StudyPlanTableView: UITableView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
